ef7932f45bbad8f74d042113fde28d2d850b5edf
LeakimDev <mikaeleakim2001@gmail.com>
2026-03-23 18:32:59 -0300

    Delete ProjetoTeste directory

--- ProjetoTeste/src/Estudantil.java
+++
@@ -1,9 +0,0 @@
-public class Estudantil {
-    public static void main(String[] args){
-        int mediaFinal = 6;
-        if (mediaFinal < 6)
-            System.out.println("Reprovado");
-        else
-            System.out.println("Aprovado");
-    }
-}
